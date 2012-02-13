03c1ff4302ac41594105f9f7e568dabfef7afd6f
rongjihuang <rongjihuang@gmail.com>
2012-02-13 18:18:01 +0800

    db.postgresql.update.v0.8-v0.9.sql

---
+++ src/main/resources/db/db.postgresql.update.v0.8-v0.9.sql
@@ -0,0 +1,26 @@
+-- ###########################################################################
+-- 宝城综合应用系统的升级脚本
+-- 数据库类型: postgresql
+-- 升级版本: 从 0.6 升级到 0.7
+-- ###########################################################################
+-- SQL参考脚本：
+-- ##列相关：
+-- 1) 删除列：ALTER TABLE 表名 DROP COLUMN 列名;
+-- 2) 添加列：ALTER TABLE 表名 ADD COLUMN 列名 数据类型 [DEFAULT 默认值];
+-- 3) 修改列的数据类型：ALTER TABLE 表名 ALTER COLUMN 列名 TYPE 新数据类型;
+-- 4) 修改列名：ALTER TABLE 表名 RENAME 列名 TO 新列名;
+-- 5) 添加列注释：COMMENT ON COLUMN 表名.列名 IS '注释';
+-- ##索引、外键相关：
+-- 1) 创建索引：CREATE INDEX 索引名 ON 表名 (列名);
+-- 2) 创建外键：ALTER TABLE 子表名 ADD CONSTRAINT 外键名 FOREIGN KEY (子表列名) REFERENCES 主表名 (主表列名) [ON DELETE CASCADE];
+-- 3) 创建唯一性约束：ALTER TABLE 表名 ADD CONSTRAINT 约束名 UNIQUE (列名);
+-- ##表、视图、函数、序列相关：
+-- 1) 改表名：ALTER TABLE 表名 RENAME TO 新表名;
+-- 2) 删表：DROP TABLE IF EXISTS 表名;
+-- 3) 删视图：DROP VIEW IF EXISTS 视图名;
+-- 4) 删序列：DROP SEQUENCE IF EXISTS 序列名;
+-- 5) 删函数：DROP FUNCTION IF EXISTS 函数名(参数1类型,参数2类型);
+-- 6) 添加表注释：COMMENT ON TABLE 表名 IS '注释';
+-- ###########################################################################
+
+-- ####   ####
