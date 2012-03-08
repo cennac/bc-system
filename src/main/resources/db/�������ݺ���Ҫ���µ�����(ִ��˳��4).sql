af7d4b7d736bad7d29fdb876bef7b5eba36c1ba9
zxr <991316914@qq.com>
2012-03-08 10:49:05 +0800

    修改添加营运班次记录

---
+++ src/main/resources/db/�������ݺ���Ҫ���µ�����(ִ��˳��4).sql
@@ -0,0 +1,44 @@
+﻿
+
+--更新司机最新的驾驶状态
+UPDATE BS_CARMAN SET CLASSES = getDriverClassesByDriverId(ID);
+
+--更新司机最新的迁移类型
+UPDATE BS_CARMAN SET MOVE_TYPE = getDriverMoveTypeByDriverId(ID);
+
+--更新司机最新的营运车辆
+UPDATE BS_CARMAN SET CARINFO = getCarInfoByDriverId(ID);
+
+--更新司机最新的迁移日期
+UPDATE BS_CARMAN SET MOVE_DATE = getDriverMoveDateByDriverId(ID);
+
+
+-- 更新在案车辆的司机段信息
+update bs_car c set driver=getdriverinfobycarid(id) where status_=0;
+
+-- 更新注销司机的营运班次(司机表的沉余字段)为空
+UPDATE BS_CARMAN SET CLASSES = 0 WHERE  STATUS_= 1;
+
+
+--更新司机最新的主车辆
+UPDATE BS_CARMAN SET MAIN_CAR_ID = getDriverMainCarIdByDriverId(ID);
+
+
+--(新增的函数)---------------------------------------------------------------------------------------------------
+--##获取顶班司机顶班合同期的结束日期##
+CREATE OR REPLACE FUNCTION getDriverEndDateByDriverId(did IN integer) RETURNS timestamp AS $$
+DECLARE
+	--定义变量
+	endDate timestamp;
+BEGIN
+	select  h.end_date
+		into endDate
+		from BS_CAR_DRIVER_HISTORY h where h.driver_id=did and h.move_type=7 order by h.move_date desc limit 1;
+	return endDate;
+END;
+$$ LANGUAGE plpgsql;
+
+--更新顶班司机顶班合同期的结束日期 shiftwork_end_date
+UPDATE BS_CARMAN SET SHIFTWORK_END_DATE = getDriverEndDateByDriverId(ID);
+
+----------------------------------------------------------------------------------------------------------------
