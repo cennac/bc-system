f9a84516a52d6884beba5179ff93604c4df340aa
zxr <991316914@qq.com>
2012-03-09 16:28:42 +0800

    更新在案的司机责任人(三月二号前创建的但最新的经济合同已注销或没有经济合同)为司机

---
+++ src/main/resources/db/��ʱ�ű�/04-�����ڰ���˾��������(���¶���ǰ�����ĵ����µľ��ú�ͬ��ע����û�о��ú�ͬ)Ϊ˾��.sql
@@ -0,0 +1,10 @@
+﻿
+--更新在案的司机责任人(三月二号前创建的但最新的经济合同已注销或没有经济合同)为司机
+update bs_carman  set type_ = 0 where id in
+	(select id from bs_carman where status_=0 and type_=2 
+		and id not in ( 
+			select man_id from bs_carman_contract 
+			where contract_id in (
+				select id from bs_contract 
+				where type_=2 and status_=0)) 
+		and to_char(file_date,'YYYY-MM-DD') < '2012-03-02')
