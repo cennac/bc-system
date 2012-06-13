4c7368412a056ae73d99026a1e85fb70724e0f8b
dragon <rongjihuang@gmail.com>
2012-06-13 18:14:56 +0800

    模板管理中没有记录附件操作日志的相关修改

--- src/main/resources/db/db.postgresql.update.v1.3-v1.3.1.sql
+++ src/main/resources/db/db.postgresql.update.v1.3-v1.3.1.sql
@@ -17,12 +17,28 @@ ALTER TABLE bc_docs_attach_history DROP COLUMN modifier_id;
 ALTER TABLE bc_docs_attach_history DROP COLUMN modified_date;
 
 -- 附件操作日志新列
-ALTER TABLE bc_docs_attach_history ALTER COLUMN aid DROP NOT NULL;
+ALTER TABLE bc_docs_attach_history ADD COLUMN ptype varchar(36);
+ALTER TABLE bc_docs_attach_history ADD COLUMN puid varchar(36);
 ALTER TABLE bc_docs_attach_history ADD COLUMN path varchar(500);
 ALTER TABLE bc_docs_attach_history ADD COLUMN apppath boolean default false;
-update bc_docs_attach_history h set path=(select a.path from bc_docs_attach a where a.id=h.aid),apppath=(select a.apppath from bc_docs_attach a where a.id=h.aid);
+update bc_docs_attach_history h set path=(select a.path from bc_docs_attach a where h.aid=a.id)
+	,apppath=(select a.apppath from bc_docs_attach a where h.aid=a.id)
+	,ptype=(select a.ptype from bc_docs_attach a where h.aid=a.id)
+	,puid=(select a.puid from bc_docs_attach a where h.aid=a.id);
+ALTER TABLE bc_docs_attach_history DROP COLUMN aid;
 ALTER TABLE bc_docs_attach_history ALTER COLUMN path SET NOT NULL;
 ALTER TABLE bc_docs_attach_history ALTER COLUMN apppath SET NOT NULL;
+ALTER TABLE bc_docs_attach_history ALTER COLUMN ptype SET NOT NULL;
+ALTER TABLE bc_docs_attach_history ALTER COLUMN puid SET NOT NULL;
 COMMENT ON COLUMN bc_docs_attach_history.path IS '物理文件保存的相对路径(相对于全局配置的附件根目录下的子路径，如"2011/bulletin/xxxx.doc")';
 COMMENT ON COLUMN bc_docs_attach_history.apppath IS '指定path的值是相对于应用部署目录下路径还是相对于全局配置的app.data目录下的路径';
 COMMENT ON COLUMN bc_docs_attach_history.format IS '附件类型:如pdf、doc、mp3等';
+COMMENT ON COLUMN bc_docs_attach_history.ptype IS '文档类型';
+COMMENT ON COLUMN bc_docs_attach_history.puid IS '文档标识';
+
+-- 模板管理添加uid列
+ALTER TABLE BC_TEMPLATE ADD COLUMN uid_ varchar(36);
+update BC_TEMPLATE set uid_='Template.mt.'||id;
+ALTER TABLE BC_TEMPLATE ALTER COLUMN uid_ SET NOT NULL;
+
+
