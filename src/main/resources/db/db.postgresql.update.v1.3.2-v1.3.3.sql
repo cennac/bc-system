b6ea73ae17a75b0b44efb1eb8e2835fded78c0b0
dragon <rongjihuang@gmail.com>
2012-06-27 16:06:46 +0800

    插入 综合业务组 岗位

--- src/main/resources/db/db.postgresql.update.v1.3.2-v1.3.3.sql
+++ src/main/resources/db/db.postgresql.update.v1.3.2-v1.3.3.sql
@@ -3,3 +3,13 @@
 -- 数据库类型: postgresql
 -- 升级版本: 从 1.3.2 升级到 1.3.3
 -- ###########################################################################
+
+-- 插入 综合业务组 岗位
+insert into BC_IDENTITY_ACTOR (ID,UID_,STATUS_,INNER_,TYPE_,CODE, NAME, ORDER_,PCODE,PNAME) 
+	select NEXTVAL('CORE_SEQUENCE'),'group.init.'||NEXTVAL('CORE_SEQUENCE'), 0, false, 3
+	, 'zongHeYeWuZu','综合业务组', '1001','[1]baochengzongbu','宝城总部'
+	from bc_dual where not exists (select 0 from BC_IDENTITY_ACTOR where code='zongHeYeWuZu');
+insert into BC_IDENTITY_ACTOR_RELATION (TYPE_,MASTER_ID,FOLLOWER_ID) 
+    select 0,am.id,af.id from BC_IDENTITY_ACTOR am,BC_IDENTITY_ACTOR af where am.code='zongHeYeWuZu' 
+	and af.code in ('may','hewl','wing')
+	and not exists (select 0 from BC_IDENTITY_ACTOR_RELATION r where r.type_=0 and r.MASTER_ID=am.id and r.FOLLOWER_ID=af.id);
