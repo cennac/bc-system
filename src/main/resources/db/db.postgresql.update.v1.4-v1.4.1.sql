f648c8d87df6a8ed28a9937bbb4d2832b869470e
dragon <rongjihuang@gmail.com>
2012-08-31 18:11:31 +0800

    修正sql脚本

--- src/main/resources/db/db.postgresql.update.v1.4-v1.4.1.sql
+++ src/main/resources/db/db.postgresql.update.v1.4-v1.4.1.sql
@@ -6,4 +6,4 @@
 
 --发票销售单增加流水号
 ALTER TABLE bs_invoice_sell ADD COLUMN code_no VARCHAR(255);
-comment on column bs_invoice_sell.code is '发票单流水号，以年计算';
+comment on column bs_invoice_sell.code_no is '发票单流水号,以年计算';
