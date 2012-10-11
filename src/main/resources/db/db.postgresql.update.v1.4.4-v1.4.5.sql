2c882673d44f91e322719732be3fcd4a0acbabc2
bryanthe <bryanthe@163.com>
2012-10-11 17:33:43 +0800

    更新流程部署版本号规则

--- src/main/resources/db/db.postgresql.update.v1.4.4-v1.4.5.sql
+++ src/main/resources/db/db.postgresql.update.v1.4.4-v1.4.5.sql
@@ -78,4 +78,4 @@ values (NEXTVAL('CORE_SEQUENCE'),'DeployResource.110',(select id from BC_WF_DEPL
 		,'InChargeDeputyGeneralManagerCheck2JS','InChargeDeputyGeneralManagerCheck2JS','generalOrder/InChargeDeputyGeneralManagerCheck2JS.js',939,'InChargeDeputyGeneralManagerCheck2JS.js',(select id from BC_TEMPLATE_TYPE where code='js'),false);
 
 -- 更新流程部署版本号规则 
-update bc_wf_deploy set version_ = version_ || '.0'
+update bc_wf_deploy set version_ = '1.0'
