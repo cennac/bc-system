a401f20204e79e88bdb73afa0f861c5cc8e7bf3b
zxr <991316914@qq.com>
2012-11-28 10:05:57 +0800

    添加车辆信息模块表添加车辆产权和插入车辆信息产权的查看角色数据sql

--- src/main/resources/db/db.postgresql.update.v1.4.8-v1.4.9.sql
+++ src/main/resources/db/db.postgresql.update.v1.4.8-v1.4.9.sql
@@ -17,3 +17,10 @@ insert into BC_IDENTITY_ROLE_RESOURCE (RID,SID)
 	select r.id,m.id from BC_IDENTITY_ROLE r,BC_IDENTITY_RESOURCE m where r.code='BS_OWNERSHIP_CHECK_ADVANCED' 
 	and m.type_ > 1 and m.order_ in ('032000')
 	order by m.order_;
+	
+--车辆信息模块表添加车辆产权
+ALTER TABLE BS_CAR ADD COLUMN OWNER_ VARCHAR(255);
+COMMENT ON COLUMN BS_CAR.OWNER_ IS '车辆产权';
+-- 插入车辆信息产权的查看角色数据
+insert into  BC_IDENTITY_ROLE (ID, STATUS_,INNER_,TYPE_,ORDER_,CODE,NAME) 
+	values(NEXTVAL('CORE_SEQUENCE'), 0, false,  0,'0102-4', 'BS_CAR_OWNER_CHECK','车辆产权查看');
