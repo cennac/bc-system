3bad57c69427ab606038ee75d49c5b3db48b3a98
fatfish <fatfish2010@sina.cn>
2013-02-06 16:57:38 +0800

    自接投诉流程和客观投诉流程js文件修改

---
+++ src/main/resources/db/bcdata.update.v1.9.4-v1.9.5/workflow/deploy/resource/complainHandle/1.0/accraditationCaseAdviceInfoAgain.js
@@ -0,0 +1,42 @@
+bc.namespace("bswf.complainHandle");
+bswf.complainHandle.accraditationCaseAdviceInfoAgainForm = {
+	
+	/** 表单验证方法 */
+	validateForm: function(){
+		var $form = $(this);
+		//获取完成时间
+		$form.find("input[type='hidden'][name='finish4Majordomo']").val("");
+		var myDate = new Date();
+		var year=myDate.getFullYear();
+		var month=myDate.getMonth()+1;
+		var date=myDate.getDate();
+		$form.find("input[type='hidden'][name='finish4Majordomo']").val(year+"年"+month+"月"+date+"日");
+		
+		if(!bc.validator.validate(this))
+			return false;
+			
+		//是否返回监督组重办
+		if($form.find("input[type='radio'][name='isReturn']").size()!=0){
+			var $rhandings=$form.find("input[type='radio'][name='isReturn']");
+			var checked=false;
+			var value;
+			$rhandings.each(function(){
+				if($(this)[0].checked){
+					value=$(this).val();
+					checked=true;
+				}
+			});
+			if(!checked){
+				bc.msg.alert("请确认下一步相关操作！");
+				return false;
+			}else{
+				$form.find(":input[type='hidden'][name='isReturn']").val(value);
+				$form.find(":input[type='hidden'][name='isReturn_lc']").val(value);
+			}
+		
+		}
+
+		return true;
+	}
+	
+};
